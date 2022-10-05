02c56a07bd3855b4e36d0ebe03b98f15893ebc1a
Ken <y23@mail.com>
2022-10-05 22:57:26 +0100

    Correct typo

--- things/models.py
+++ things/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Thing(models.Model):
     name = models.TextField()
-    description: models.TextField()
+    description = models.TextField()
     quantity = models.IntegerField()
