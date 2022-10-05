6e2e02f186c6049be592067ef903590ce256b1cb
Ken <y23@mail.com>
2022-10-05 23:03:57 +0100

    Refine model Thing

--- things/models.py
+++ things/models.py
@@ -1,7 +1,18 @@
+from math import fabs
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(
+        max_length = 30,
+        unique = True,
+    )
+    description = models.CharField(
+        max_length = 120,
+        unique = False,
+    )
+    quantity = models.IntegerField(
+        unique = False,
+        regex = r'^([0-9]|[1-9][0-9]|100)$',
+    )
